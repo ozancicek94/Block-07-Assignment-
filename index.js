d94653991b3c37f29189a5d598bfb68dd42c9e04
Ozan Cicek <ozancicek@Ozans-MBP.lan>
2024-04-29 23:48:15 -0400

    Block 07 Assignment added

---
+++ index.js
@@ -0,0 +1,14 @@
+// Here, I will declare three variables that are going to give the result in numbers 10-40-39.
+
+let firstNum = ((((100/20)*4)+5)-15);
+let secondNum = ((((5+15)*5)/2) - 10);
+let thirdNum = ((7*6)-3);
+
+// Here, I will declare the "string" variable, that forms the beginning part of the sentence.
+
+let string = `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`;
+
+// Here, I will create the alert that combines the sentence with the variables defined in lines 3-5.
+
+alert(string + `${firstNum} - ${secondNum} - ${thirdNum}`);
+
